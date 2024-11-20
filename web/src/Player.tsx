0ed2841929dfebdab71ff99719ab911bc0ca38b4
Eliza Zhang <eliza@elizazhang.com>
2024-11-20 06:20:09 -0800

    Holy shit, it finally works

--- web/src/Player.tsx
+++ web/src/Player.tsx
@@ -48,15 +48,27 @@ export default function Player(props: PlayerProps) {
 
   const playerId = useId();
   const playerRef = useRef<any>(null);
-  const [playerLoaded, setPlayerLoaded] = useState(false);
   const [playerReady, setPlayerReady] = useState(false);
 
-  // State tracking for old props.
+  // This insane hack uses a Ref in order to store a map to props that are used
+  // in the player's event handlers. This is required because the event handlers
+  // are set on construction, and only run exactly once, which means they only
+  // ever close over the first snapshot of prop values. If we don't use a ref
+  // here, the prop values (e.g. `videoId`) referenced in the event handlers
+  // will always be their _initial_ values, not their latest values. This map is
+  // an indirection trick so that the handlers can reference the latest values.
+  const propsRef = useRef<Record<string, any>>({});
+  propsRef.current["videoId"] = videoId;
+  propsRef.current["playing"] = playing;
+  propsRef.current["onReady"] = onReady;
+  propsRef.current["onEnded"] = onEnded;
+  propsRef.current["onPlay"] = onPlay;
+  propsRef.current["onPause"] = onPause;
+  propsRef.current["onAutoplayBlocked"] = onAutoplayBlocked;
+  propsRef.current["onError"] = onError;
+
+  // State tracking for old props where setting has side effects.
   const [lastVideoId, setLastVideoId] = useState("");
-  const [lastOnReady, setLastOnReady] = useState<any>(undefined);
-  const [lastOnStateChange, setLastOnStateChange] = useState<any>(undefined);
-  const [lastOnAutoplayBlocked, setLastOnAutoplayBlocked] = useState<any>(undefined);
-  const [lastOnError, setLastOnError] = useState<any>(undefined);
 
   // Initialize the player.
   useEffect(() => {
@@ -72,27 +84,58 @@ export default function Player(props: PlayerProps) {
           controls: 0,
           autoplay: 0,
         },
+        events: {
+          onReady(e: { target: PlayerController }) {
+            // I know this looks insane but I promise it's needed, because some
+            // of the methods on the player (like `loadVideoById`) don't exist
+            // after construction. Apparently, they're added asynchronously.
+            //
+            // You can observe this behavior with:
+            //
+            // > setInterval(() => console.log(Object.keys(playerRef.current)), 100);
+            //
+            // on the player immediately after construction, and watch as the
+            // object's keys change. It is truly baffling. I spent so fucking
+            // long debugging this.
+            const interval = setInterval(() => {
+              if (Object.keys(playerRef.current).includes("loadVideoById")) {
+                clearInterval(interval);
+
+                if (propsRef.current?.["videoId"]) {
+                  if (propsRef.current?.["playing"]) {
+                    console.log("load vid");
+                    playerRef.current.loadVideoById(videoId);
+                  } else {
+                    console.log("cue vid");
+                    playerRef.current.cueVideoById(videoId);
+                  }
+                  setLastVideoId(videoId);
+                }
+                propsRef.current?.["onReady"]?.(e);
+                setPlayerReady(true);
+              }
+            }, 1000);
+          },
+          onStateChange(e: { data: number }) {
+            if (e.data === 0) {
+              propsRef.current?.["onEnded"]?.();
+            } else if (e.data === 1) {
+              propsRef.current?.["onPlay"]?.();
+            } else if (e.data === 2) {
+              propsRef.current?.["onPause"]?.();
+            }
+          },
+          onAutoplayBlocked() {
+            propsRef.current?.["onAutoplayBlocked"]?.();
+          },
+          onError(e: { data: number }) {
+            propsRef.current?.["onError"]?.(e);
+          },
+        },
       });
       if (debugging()) {
         (window as any).player = playerRef.current;
       }
-      // I know this looks insane but I promise it's needed, because some of the
-      // methods on the player (like `loadVideoById`) don't exist after
-      // construction. Apparently, they're added asynchronously.
-      //
-      // You can observe this behavior with:
-      //
-      // > setInterval(() => console.log(Object.keys(playerRef.current)), 100);
-      //
-      // on the player immediately after construction, and watch as the object's
-      // keys change. It is truly baffling. I spent so fucking long debugging
-      // this.
-      const interval = setInterval(() => {
-        if (Object.keys(playerRef.current).includes("loadVideoById")) {
-          clearInterval(interval);
-          setPlayerLoaded(true);
-        }
-      }, 500);
     }
     window.addEventListener("load", () => {
       debug("load");
@@ -111,98 +154,7 @@ export default function Player(props: PlayerProps) {
     if ((window as any).YT.loaded === 1) {
       (window as any).onYouTubeIframeAPIReady();
     }
-    // TODO: The problem is that this only runs one time, and does not run with
-    // the newer versions of the callback props, which close over the newer
-    // state values.
-    //
-    // I need to add the callback props as reactive dependencies, and figure out
-    // a way to not re-initialize the player if it's been created, while also
-    // adding the new callbacks to listen to the events.
-    //
-    // Current idea:
-    // - Use the setInterval trick instead of onReady.
-    // - Add callbacks as event listeners.
-    // - useState to store old props so I can unlisten them on new props.
   }, []);
-
-  // Synchronize player with event listeners.
-  useEffect(() => {
-    console.log("useEffect: [playerLoaded, onReady, lastOnReady]", { playerLoaded, onReady, lastOnReady });
-    if (playerLoaded && playerRef.current) {
-      const onReadyWrapped = (e: { target: PlayerController }) => {
-        console.log(e)
-        if (e === undefined) {
-          return;
-        }
-        if (videoId) {
-          if (playing) {
-            console.log("load vid");
-            playerRef.current.loadVideoById(videoId);
-          } else {
-            console.log("cue vid");
-            playerRef.current.cueVideoById(videoId);
-          }
-          setLastVideoId(videoId);
-        }
-        setPlayerReady(true);
-        onReady?.(e);
-      };
-      playerRef.current.removeEventListener("onReady", lastOnReady);
-      setLastOnReady(onReadyWrapped);
-      playerRef.current.addEventListener("onReady", onReadyWrapped);
-      return () => playerRef.current.removeEventListener("onReady", onReadyWrapped);
-    }
-  }, [playerLoaded, onReady, lastOnReady, videoId, playing]);
-  useEffect(() => {
-    if (playerReady && playerRef.current) {
-      const onStateChangeWrapped = (e: { data: number }) => {
-        console.log(e)
-        if (e === undefined) {
-          return;
-        }
-        if (e.data === 0) {
-          onEnded?.();
-        } else if (e.data === 1) {
-          onPlay?.();
-        } else if (e.data === 2) {
-          onPause?.();
-        }
-      };
-      playerRef.current.removeEventListener("onStateChange", lastOnStateChange);
-      setLastOnStateChange(onStateChangeWrapped);
-      playerRef.current.addEventListener("onStateChange", onStateChangeWrapped);
-      return () => playerRef.current.removeEventListener("onStateChange", onStateChangeWrapped);
-    }
-    // TODO: Now I'm missing the ready event. Hmm, is there a way I can set the
-    // event handlers in the beginning, but call out to the latest prop
-    // callbacks? Using mutable indirection... maybe a ref to a map? That should
-    // keep stable object identity, I think.
-  }, [playerReady, onPlay, onPause, onEnded, lastOnStateChange]);
-  useEffect(() => {
-    if (playerReady && playerRef.current) {
-      const onAutoplayBlockedWrapped = () => {
-        onAutoplayBlocked?.();
-      };
-      playerRef.current.removeEventListener("onAutoplayBlocked", lastOnAutoplayBlocked);
-      setLastOnAutoplayBlocked(onAutoplayBlockedWrapped);
-      playerRef.current.addEventListener("onAutoplayBlocked", onAutoplayBlockedWrapped);
-      return () => playerRef.current.removeEventListener("onAutoplayBlocked", onAutoplayBlockedWrapped);
-    }
-  }, [playerReady, onAutoplayBlocked, lastOnAutoplayBlocked]);
-  useEffect(() => {
-    if (playerReady && playerRef.current) {
-      const onErrorWrapped = (e: { data: number }) => {
-        console.log(e)
-        if (e === undefined) {
-          return;
-        }
-        onError?.(e);
-      };
-      playerRef.current.removeEventListener("onError", lastOnError);
-      setLastOnError(onErrorWrapped);
-      playerRef.current.addEventListener("onError", onErrorWrapped);
-    }
-  }, [playerReady, onError, lastOnError]);
 
   // Synchronize player state with various props.
   useEffect(() => {
